aad82163cafa3c6bfcc554de07c45b9594c47296
touyaA142 <touya212codegym@gmail.com>
2021-10-23 16:38:44 +0900

    test

--- project_help/settings.py
+++ project_help/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'team_development',
-        'USER': 'postgres',
-        'PASSWORD': 'touya212',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': '',
         'POST': '',
     }
