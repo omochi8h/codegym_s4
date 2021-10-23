760885010191ba68255c81b9ae4804e519b8c59f
maih121120 <mai.h121120@gmail.com>
2021-10-23 14:30:04 +0900

    hirai_1023_1429

---
+++ static/js/script.js
@@ -0,0 +1,49 @@
+ function close() {
+  document.getElementById('modal').classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+}
+
+function open() {
+  document.getElementById('modal').classList.add("active");
+  document.getElementById("mask").classList.add("active");
+}
+
+function close1() {
+  document.getElementById('modal1').classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+}
+
+function open1() {
+  document.getElementById('modal1').classList.add("active");
+  document.getElementById("mask").classList.add("active");
+}
+
+function close2() {
+  document.getElementById('modal2').classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+}
+
+function open2() {
+  document.getElementById('modal2').classList.add("active");
+  document.getElementById("mask").classList.add("active");
+}
+
+function close3() {
+  document.getElementById('modal3').classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+}
+
+function open3() {
+  document.getElementById('modal3').classList.add("active");
+  document.getElementById("mask").classList.add("active");
+}
+
+function close4() {
+  document.getElementById('modal4').classList.remove("active");
+  document.getElementById("mask").classList.remove("active");
+}
+
+function open4() {
+  document.getElementById('modal4').classList.add("active");
+  document.getElementById("mask").classList.add("active");
+}
